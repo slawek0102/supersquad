8305e462d8a16edbe972e79f1b580faa3232c1a0
Your Name <grzebyk.slawomir@gmail.com>
2018-09-01 17:26:28 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Button} from 'antd'
+
+import './App.css';
+
+class App extends Component {
+
+    render() {
+
+
+        return (
+            <div className="App">
+
+            div
+
+            </div>
+        );
+    }
+}
+
+export default App;
